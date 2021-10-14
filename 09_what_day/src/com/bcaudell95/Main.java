b814531e67a73f6a402186511833d2bce6bd2590
bcaudell95 <bcaudell95@gmail.com>
2021-10-13 20:57:17 -0700

    Challenge 09 problem

---
+++ 09_what_day/src/com/bcaudell95/Main.java
@@ -0,0 +1,11 @@
+// [2017-10-30] Challenge #338 [Easy] What day was it again?
+// https://www.reddit.com/r/dailyprogrammer/comments/79npf9/20171030_challenge_338_easy_what_day_was_it_again/
+// Suggested language: Java
+package com.bcaudell95;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
